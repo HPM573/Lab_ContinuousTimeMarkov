32eecd2dd5f67dbbf2213b10536a91c31f01aafe
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2019-02-04 13:07:00 -0500

    calculating rate matrix

---
+++ InputData.py
@@ -0,0 +1,68 @@
+import numpy as np
+from enum import Enum
+import SimPy.MarkovClasses as Markov
+
+# simulation settings
+POP_SIZE = 5000         # cohort population size
+SIM_TIME_STEPS = 100    # length of simulation (years)
+# annual probability of background mortality (number per year per 1,000 population)
+ANNUAL_PROB_BACKGROUND_MORT = 8.15 / 1000
+
+# transition matrix
+TRANS_MATRIX = [
+    [1251,  350,    116,    17],   # CD4_200to500
+    [0,     731,    512,    15],   # CD4_200
+    [0,     0,      1312,   437]   # AIDS
+    ]
+
+
+class HealthState(Enum):
+    """ health states of patients with HIV """
+    CD4_200to500 = 0
+    CD4_200 = 1
+    AIDS = 2
+    HIV_DEATH = 3
+    NATUAL_DEATH = 4
+
+
+def get_trans_prob_matrix(trans_matrix):
+    """
+    :param trans_matrix: transition matrix containing counts of transitions between states
+    :return: transition probability matrix
+    """
+
+    # initialize transition probability matrix
+    trans_prob_matrix = []
+
+    # for each row in the transition matrix
+    for row in trans_matrix:
+        # calculate the transition probabilities
+        prob_row = np.array(row)/sum(row)
+        # add this row of transition probabilities to the transition probability matrix
+        trans_prob_matrix.append(prob_row)
+
+    return trans_prob_matrix
+
+
+def get_trans_rate_matrix(trans_matrix):
+
+    # find the transition probability matrix
+    trans_prob_matrix = get_trans_prob_matrix(trans_matrix=trans_matrix)
+
+    # find the transition rate matrix
+    trans_rate_matrix = Markov.discrete_to_continuous(
+        prob_matrix=trans_prob_matrix,
+        delta_t=1)
+
+    # calculate background mortality rate
+    mortality_rate = -np.log(1 - ANNUAL_PROB_BACKGROUND_MORT)
+
+    # add background mortality rate
+    for row in trans_rate_matrix:
+        row.append(mortality_rate)
+
+    # add 2 rows for HIV death and natural death
+    trans_rate_matrix.append([0] * len(HealthState))
+    trans_rate_matrix.append([0] * len(HealthState))
+
+    return trans_rate_matrix
---
+++ RunMarkovModel.py
@@ -0,0 +1,4 @@
+import InputData as D
+
+
+print(D.get_trans_rate_matrix(D.TRANS_MATRIX))
